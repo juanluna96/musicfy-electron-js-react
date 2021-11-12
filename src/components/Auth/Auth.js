070a1acb8841458cc208ae86fc150ae9511fcd60
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-11 22:00:07 -0500

    Página para seleccionar Login o Registro de usuario 2/2

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 import Registerform from '../Forms/RegisterForm';
 import Loginform from '../Forms/LoginForm';
 import AuthOptions from './AuthOptions/AuthOptions';
+import BackgroundAuth from '../../assets/img/jpg/background-auth.jpg'
+import LogoBlackWhite from '../../assets/img/png/logo-name-white.png'
+
+import "./Auth.scss";
 
 const Auth = () => {
     const [selectedForm, setSelectedForm] = useState(null);
@@ -24,9 +28,15 @@ const Auth = () => {
     }
 
     return (
-        <div>
-            <h1>Iniciar Sesion</h1>
-            <h2>Registrarse</h2>
+        <div className="auth" style={ { backgroundImage: `url(${BackgroundAuth})` } }>
+            <div className="auth__dark">
+                <div className="auth__box">
+                    <div className="auth__box-logo">
+                        <img src={ LogoBlackWhite } alt="MusicFY" />
+                    </div>
+                    { handleForm() }
+                </div>
+            </div>
         </div>
     )
 }
