d55459463f1a7f4b03997eae446e0111e2c56f24
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-17 20:51:19 -0500

    Validando formulario de login

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -16,6 +16,10 @@ const Loginform = ({ setSelectedForm }) => {
 
     const [showPassword, setShowPassword] = useState(false);
     const [formLogin, setFormLogin] = useState(formLoginDefault);
+    const [error, setError] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [userActive, setUserActive] = useState(false);
+    const [user, setUser] = useState(null);
 
     const changePasswordType = () => {
         setShowPassword(!showPassword);
@@ -30,26 +34,71 @@ const Loginform = ({ setSelectedForm }) => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(formLogin)
+        setLoading(true)
+        setError({});
+        let errors = {};
+        let formOk = true;
+
+
+        if (!validateEmail(formLogin.email)) {
+            errors.email = true;
+            formOk = false;
+        }
+
+        if (formLogin.password.length < 6) {
+            errors.password = true;
+            formOk = false;
+        }
+
+        setError(errors);
+
+        if (formOk) {
+            firebase.auth().signInWithEmailAndPassword(formLogin.email, formLogin.password)
+                .then(res => {
+                    setLoading(false);
+                    setUserActive(true);
+                    setUser(res.user);
+                    setSelectedForm('home');
+                })
+                .catch(err => {
+                    setLoading(false);
+                    toast.error(err.message);
+                })
+        }
+
+        setLoading(false);
     }
 
     return (
         <div className="login-form">
-            <h1>Musica para todos.</h1>
+            <h1>Musica para todos</h1>
+            <h1>{ loading ? 'Loading' : 'Not loading' }</h1>
 
             <Form onSubmit={ onSubmit } onChange={ onChange }>
                 <Form.Field>
-                    <Input icon='user' name='email' placeholder='Correo electrónico' />
+                    <Input icon='user' name='email' placeholder='Correo electrónico' error={ error.email } />
+                    {
+                        error.email &&
+                        <span className="error-text">
+                            El correo electrónico no es válido
+                        </span>
+                    }
                 </Form.Field>
                 <Form.Field>
                     <Input type={ showPassword ? 'text' : 'password' } name="password" placeholder="Contraseña"
-                        // error={ error.password }
+                        error={ error.password }
                         icon={
                             <Icon name={ showPassword ? 'eye slash' : 'eye' } link onClick={ () => changePasswordType() } />
                         }
                     />
+                    {
+                        error.password &&
+                        <span className="error-text">
+                            La contraseña debe tener al menos 6 caracteres
+                        </span>
+                    }
                 </Form.Field>
-                <Button type='submit' >Iniciar sesion</Button>
+                <Button type='submit' loading={ loading }>Iniciar sesion</Button>
             </Form>
 
             <div className="login-form__options">
