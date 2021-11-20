2521cdfa1518838343904ef96eb51f2dc41812ee
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-19 21:24:23 -0500

    Creando sistemas de rutas con React Router Dom

--- src/layouts/LoggedLayout/LoggedLayout.js
+++ src/layouts/LoggedLayout/LoggedLayout.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react';
+import { BrowserRouter } from 'react-router-dom';
 
 import "./Loggedlayout.scss"
 
+import RouterComponent from '../../routes/Routes';
+
 const Loggedlayout = ({ user }) => {
     return (
-        <Grid className="logged-layout">
-            <Grid.Row>
-                <Grid.Column width={ 3 }>
-                    <h2>Logged Layout</h2>
-                </Grid.Column>
-                <Grid.Column className="content" width={ 13 }>
-                    <h2>TopBar</h2>
-                    <h2>SideBar</h2>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column width={ 16 }>
-                    <h2>Player</h2>
-                </Grid.Column>
-            </  Grid.Row>
-        </Grid>
+        <BrowserRouter>
+            <Grid className="logged-layout">
+                <Grid.Row>
+                    <Grid.Column width={ 3 }>
+                        <h2>Logged Layout</h2>
+                    </Grid.Column>
+                    <Grid.Column className="content" width={ 13 }>
+                        <h2>TopBar</h2>
+                        <RouterComponent />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={ 16 }>
+                        <h2>Player</h2>
+                    </Grid.Column>
+                </  Grid.Row>
+            </Grid>
+        </BrowserRouter>
     )
 }
 
