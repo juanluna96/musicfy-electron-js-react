c5b025d5a2b873df686b0813a9941ae083434761
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-21 10:52:40 -0500

    Sistema para mostrar el menu activo en todo momento

--- src/components/Menu/Left/MenuLeft.js
+++ src/components/Menu/Left/MenuLeft.js
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./MenuLeft.scss"
 
 const Menuleft = ({ user }) => {
+    const location = useLocation()
+
+    const [activeMenu, setActiveMenu] = useState(location.pathname);
+
+    const handleMenu = (e, menu) => {
+        setActiveMenu(menu.to);
+    }
 
     return (
         <Menu className="menu-left" vertical>
             <div className="top">
-                <Menu.Item as={ Link } to="/" name="home">
+                <Menu.Item as={ Link } to="/" active={ activeMenu === '/' } onClick={ handleMenu } name="home">
                     <Icon name="home" />Inicio
                 </Menu.Item>
-                <Menu.Item as={ Link } to="/artist" name="artist" >
+                <Menu.Item as={ Link } to="/artist" active={ activeMenu === '/artist' } onClick={ handleMenu } name="artist" >
                     <Icon name="music" />Artistas
                 </Menu.Item>
             </div>
