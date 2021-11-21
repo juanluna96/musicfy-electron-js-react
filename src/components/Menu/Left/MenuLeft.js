c40e6e0fd64e4f567ff0aeae7f894612946c2051
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-21 18:23:44 -0500

    Componente BasicModal

--- src/components/Menu/Left/MenuLeft.js
+++ src/components/Menu/Left/MenuLeft.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
 import { Link, useLocation } from "react-router-dom";
+import BasicModal from '../../Modal/BasicModal';
 
 import "./MenuLeft.scss"
 import { isUserAdmin } from '../../../db/Firestore';
@@ -22,30 +23,35 @@ const Menuleft = ({ user }) => {
     }
 
     return (
-        <Menu className="menu-left" vertical>
-            <div className="top">
-                <Menu.Item as={ Link } to="/" active={ activeMenu === '/' } onClick={ handleMenu } name="home">
-                    <Icon name="home" />Inicio
-                </Menu.Item>
-                <Menu.Item as={ Link } to="/artist" active={ activeMenu === '/artist' } onClick={ handleMenu } name="artist" >
-                    <Icon name="music" />Artistas
-                </Menu.Item>
-            </div>
-            <div className="footer">
-                {
-                    userAdmin && (
-                        <>
-                            <Menu.Item>
-                                <Icon name="plus square outline" />Nueva cancion
-                            </Menu.Item>
-                            <Menu.Item>
-                                <Icon name="plus square outline" />Nuevo artista
-                            </Menu.Item>
-                        </>
-                    )
-                }
-            </div>
-        </Menu>
+        <>
+            <Menu className="menu-left" vertical>
+                <div className="top">
+                    <Menu.Item as={ Link } to="/" active={ activeMenu === '/' } onClick={ handleMenu } name="home">
+                        <Icon name="home" />Inicio
+                    </Menu.Item>
+                    <Menu.Item as={ Link } to="/artist" active={ activeMenu === '/artist' } onClick={ handleMenu } name="artist" >
+                        <Icon name="music" />Artistas
+                    </Menu.Item>
+                </div>
+                <div className="footer">
+                    {
+                        userAdmin && (
+                            <>
+                                <Menu.Item>
+                                    <Icon name="plus square outline" />Nueva cancion
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <Icon name="plus square outline" />Nuevo artista
+                                </Menu.Item>
+                            </>
+                        )
+                    }
+                </div>
+            </Menu>
+            <BasicModal show={ true } setShow={ null } title="test">
+                <h2>Contenido del modal</h2>
+            </BasicModal>
+        </>
     )
 }
 
