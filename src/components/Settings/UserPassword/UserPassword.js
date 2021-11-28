fe1ab70c2e2cf02294e722499f4a5a414688af84
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-28 11:43:37 -0500

    Creando formulario para cambiar la contraseña y abriéndolo en el modal

--- src/components/Settings/UserPassword/UserPassword.js
+++ src/components/Settings/UserPassword/UserPassword.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Form, Icon, Input } from 'semantic-ui-react'
 
-const UserPassword = () => {
+const UserPassword = ({ user, setModalOpen, setTitleModal, setContentModal }) => {
     const onEdit = () => {
-        console.log('Editar contraseña')
+        const formComponent = <ChangePasswordForm />
+        setModalOpen(true)
+        setTitleModal('Cambiar contraseña')
+        setContentModal(formComponent)
     }
 
     return (
@@ -11,6 +14,65 @@ const UserPassword = () => {
             <h3>Contraseña: **** **** ***</h3>
             <Button circular onClick={ onEdit }>Editar</Button>
         </div>
+    )
+}
+
+const ChangePasswordForm = () => {
+    const [formData, setFormData] = useState({
+        password: '',
+        newPassword: '',
+        confirmPassword: ''
+    })
+
+    const [passwordType, setPasswordType] = useState({
+        password: true,
+        newPassword: true,
+        confirmPassword: true
+    })
+
+    const onSubmit = () => {
+        console.log(formData)
+    }
+
+
+    const onChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const changePasswordType = (field) => {
+        setPasswordType({
+            ...passwordType,
+            [field]: !passwordType[field]
+        })
+    }
+    return (
+        <Form onSubmit={ onSubmit } onChange={ onChange }>
+            <Form.Field>
+                <Input type={ passwordType.password ? 'password' : 'text' } name="password" placeholder='Contraseña actual' icon={
+                    <Icon name={ passwordType.password ? 'eye' : 'eye slash outline' } link onClick={
+                        () => changePasswordType('password')
+                    } />
+                } />
+            </Form.Field>
+            <Form.Field>
+                <Input type={ passwordType.newPassword ? 'password' : 'text' } name="newPassword" placeholder='Nueva contraseña' icon={
+                    <Icon name={ passwordType.newPassword ? 'eye' : 'eye slash outline' } link onClick={
+                        () => changePasswordType('newPassword')
+                    } />
+                } />
+            </Form.Field>
+            <Form.Field>
+                <Input type={ passwordType.confirmPassword ? 'password' : 'text' } name="confirmPassword" placeholder='Repetir nueva contraseña' icon={
+                    <Icon name={ passwordType.confirmPassword ? 'eye' : 'eye slash outline' } link onClick={
+                        () => changePasswordType('confirmPassword')
+                    } />
+                } />
+            </Form.Field>
+            <Button type='submit'>Cambiar contraseña</Button>
+        </Form>
     )
 }
 
