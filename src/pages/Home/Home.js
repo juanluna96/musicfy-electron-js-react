7d66e3adc717428fd0426699b0bded967d1ec2fc
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-11-30 13:58:15 -0500

    Obteniendo todos los artistas de Firebase

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BannerHome from '../../components/layouts/BannerHome';
+import firebase from '../../db/Firebase';
+import 'firebase/compat/firestore';
 
 import './Home.scss';
 
+const db = firebase.firestore(firebase);
+
 const Home = () => {
+    const [artists, setArtists] = useState([]);
+
+    useEffect(() => {
+        db.collection('artists').get().then(snapshot => {
+            const data = snapshot?.docs.map(doc => doc.data());
+            setArtists(data);
+        });
+    }, []);
+
     return (
         <>
             <BannerHome />
