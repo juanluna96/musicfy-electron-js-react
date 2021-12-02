471b773463b18177aa3a0da69a7a87259e7e019a
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-12-02 13:20:55 -0500

    Obteniendo las imágenes de todos los artistas

--- src/pages/Artists/Artists.js
+++ src/pages/Artists/Artists.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Grid } from 'semantic-ui-react';
 
 import firebase from '../../db/Firebase';
 import 'firebase/compat/firestore';
@@ -26,7 +27,32 @@ const Artists = () => {
 
     return (
         <div className="artists">
-            <h1>Artists</h1>
+            {
+                artists.map(artist => (
+                    <Artist key={ artist.id } artist={ artist } />
+                ))
+            }
+        </div>
+    )
+}
+
+const Artist = ({ artist }) => {
+    const [banner, setBanner] = useState(null);
+
+    useEffect(() => {
+        const storage = firebase.storage();
+        const storageRef = storage.ref();
+        const bannerRef = storageRef.child(`artists/${artist.banner}`);
+
+        bannerRef.getDownloadURL().then(url => {
+            setBanner(url);
+        });
+    }, [artist]);
+
+    return (
+        <div className="artist">
+            <h2>{ artist.name }</h2>
+            <p>{ artist.description }</p>
         </div>
     )
 }
