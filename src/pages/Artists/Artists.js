65cc9246351eabece729b14a791401b17624c23d
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-12-02 13:13:46 -0500

    Obteniendo todos los artistas de Firebase

--- src/pages/Artists/Artists.js
+++ src/pages/Artists/Artists.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import firebase from '../../db/Firebase';
+import 'firebase/compat/firestore';
 
 import './Artists.scss';
 
+const db = firebase.firestore(firebase);
+
 const Artists = () => {
+    const [artists, setArtists] = useState([]);
+
+    useEffect(() => {
+        db.collection('artists')
+            .get()
+            .then(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => (
+                    {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+                ));
+                setArtists(data);
+            });
+    }, []);
+
     return (
         <div className="artists">
             <h1>Artists</h1>
