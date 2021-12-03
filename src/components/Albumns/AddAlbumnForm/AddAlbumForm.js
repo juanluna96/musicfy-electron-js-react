bd5cb04a69a243579a20466404b17bb8212d4cbc
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-12-02 19:52:51 -0500

    Añadiendo Artistas disponibles al Dropdown

--- src/components/Albumns/AddAlbumnForm/AddAlbumForm.js
+++ src/components/Albumns/AddAlbumnForm/AddAlbumForm.js
@@ -3,12 +3,33 @@ import { Form, Input, Button, Image, Dropdown } from 'semantic-ui-react'
 import { useDropzone } from 'react-dropzone';
 import { toast } from 'react-toastify';
 import { genders } from '../../../constantes';
+import firebase from '../../../db/Firebase';
+import 'firebase/compat/firestore';
 
 import './AddAlbumForm.scss';
+
+const db = firebase.firestore();
 
 const AddAlbumForm = () => {
     const [albumImage, setAlbumImage] = useState(null);
     const [file, setFile] = useState(null);
+    const [artists, setArtists] = useState([]);
+
+    useEffect(() => {
+        const artistsRef = db.collection('artists');
+        const unsubscribe = artistsRef.onSnapshot((snapshot) => {
+            const artists = snapshot.docs.map(doc => (
+                {
+                    key: doc.id,
+                    text: doc.data().name,
+                    value: doc.data().name,
+                }
+            ));
+            console.log(artists)
+            setArtists(artists);
+        });
+        return () => unsubscribe();
+    }, []);
 
     const onSubmit = () => {
         return
@@ -43,8 +64,9 @@ const AddAlbumForm = () => {
                     </div>
                 </Form.Field>
                 <Form.Field className="album-inputs" width={ 11 }>
-                    <Input placeholder="Album Name" />
-                    <Dropdown placeholder='Genre' fluid search selection options={ genders } />
+                    <Input placeholder="Nombre del album" name="nombre" />
+                    <Dropdown placeholder='Artista' name="artista" lazyload fluid search selection options={ artists } />
+                    <Dropdown placeholder='Genero' name="genero" lazyload fluid search selection options={ genders } />
                 </Form.Field>
             </Form.Group>
             <Button type='submit'>Submit</Button>
