93ff9995e62bbafdcd2c8aa8f034dea3f8dff1a8
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-12-02 19:33:07 -0500

    Añadiendo useDropzone para el avatar el album

--- src/components/Albumns/AddAlbumnForm/AddAlbumForm.js
+++ src/components/Albumns/AddAlbumnForm/AddAlbumForm.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Form, Input, Button, Image, Dropdown } from 'semantic-ui-react'
-import { useDropzone } from 'react-dropzone'
+import { useDropzone } from 'react-dropzone';
+import { toast } from 'react-toastify';
+import { genders } from '../../../constantes';
 
 import './AddAlbumForm.scss';
 
 const AddAlbumForm = () => {
+    const [albumImage, setAlbumImage] = useState(null);
+    const [file, setFile] = useState(null);
 
     const onSubmit = () => {
         return
     }
 
-    const onDrop = () => {
-        return
-    }
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
+        setFile(file);
+        setAlbumImage(URL.createObjectURL(file));
+    })
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         accept: 'image/*',
+        noKeyboard: true,
         onDrop,
     })
 
@@ -23,23 +30,21 @@ const AddAlbumForm = () => {
         <Form className="add-album-form" onSubmit={ onSubmit }>
             <Form.Group>
                 <Form.Field className="album-avatar" width={ 5 }>
-                    <h2>AVATAR</h2>
+                    <div
+                        { ...getRootProps() }
+                        className="avatar"
+                        style={ { backgroundImage: `url(${albumImage})` } }
+                    >
+                        <input { ...getInputProps() } />
+                        {
+                            !albumImage &&
+                            <Image src={ process.env.PUBLIC_URL + '/img/no-image.png' } />
+                        }
+                    </div>
                 </Form.Field>
                 <Form.Field className="album-inputs" width={ 11 }>
                     <Input placeholder="Album Name" />
-                    <Dropdown placeholder='Genre' fluid search selection options={ [
-                        { key: '1', text: 'Rock', value: 'Rock' },
-                        { key: '2', text: 'Pop', value: 'Pop' },
-                        { key: '3', text: 'Jazz', value: 'Jazz' },
-                        { key: '4', text: 'Country', value: 'Country' },
-                        { key: '5', text: 'Hip Hop', value: 'Hip Hop' },
-                        { key: '6', text: 'Electronic', value: 'Electronic' },
-                        { key: '7', text: 'Folk', value: 'Folk' },
-                        { key: '8', text: 'Classical', value: 'Classical' },
-                        { key: '9', text: 'Blues', value: 'Blues' },
-                        { key: '10', text: 'Reggae', value: 'Reggae' },
-                        { key: '11', text: 'Other', value: 'Other' },
-                    ] } />
+                    <Dropdown placeholder='Genre' fluid search selection options={ genders } />
                 </Form.Field>
             </Form.Group>
             <Button type='submit'>Submit</Button>
