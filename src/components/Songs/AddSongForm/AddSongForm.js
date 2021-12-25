58288b172090014ca625d97fa1840b63cd79de8d
Juan Diego Luna <juandluna17@gmail.com>
2021-12-25 11:00:30 -0500

    Estructura del formulario

--- src/components/Songs/AddSongForm/AddSongForm.js
+++ src/components/Songs/AddSongForm/AddSongForm.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Form, Input, Button, Icon, Dropdown, Label } from 'semantic-ui-react';
 
 import './AddSongForm.scss';
 
 const AddSongForm = ({ showModal }) => {
+
+    const albums = [
+        { key: 'af', value: 'af', text: 'Afghanistan' },
+        { key: 'ax', value: 'ax', text: 'Aland Islands' },
+        { key: 'al', value: 'al', text: 'Albania' }
+    ]
+
+    const onSubmit = () => {
+        console.log('Submit');
+    }
+
     return (
-        <div>
-            Hola
-        </div>
+        <Form className="add-song-form" onSubmit={ onSubmit }>
+            <Form.Field>
+                <Input placeholder='Nombre de la cancion' />
+            </Form.Field>
+            <Form.Field>
+                <Dropdown placeholder='Asigna la cancion a un album' search lazyload fluid selection options={ albums } />
+            </Form.Field>
+            <Form.Field>
+                <Label>Subir cancion dropzone</Label>
+            </Form.Field>
+            <Button type='submit' content='Agregar cancion' />
+        </Form>
     )
 }
 
