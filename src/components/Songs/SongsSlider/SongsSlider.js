1dc1aee9bf8ebbe3bfa0538a6a961cc3bac7bc14
Juan Diego Luna <juandluna17@gmail.com>
2021-12-26 18:31:18 -0500

    Creando el slider de canciones

--- src/components/Songs/SongsSlider/SongsSlider.js
+++ src/components/Songs/SongsSlider/SongsSlider.js
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import firebase from '../../../db/Firebase';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
 
 import './SongsSlider.scss';
 
 const SongsSlider = ({ title, data }) => {
+    const settings = {
+        dots: false,
+        infinite: data.length > 3,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        className: 'songs-slider__list'
+    };
+
+    if (data.length === 0) {
+        return null
+    }
+
     return (
         <div className='songs-slider'>
             <h2>{ title }</h2>
-            <p>Slider</p>
+            {/* Songs slider */ }
+            <Slider { ...settings }>
+                {
+                    data.map(song => (
+                        <SongItem key={ song.id } song={ song } />
+                    ))
+                }
+            </Slider>
+        </div>
+    )
+}
+
+const SongItem = ({ song }) => {
+    const [albumImage, setAlbumImage] = useState(null);
+
+    // Get album of the song and get the image of the album
+    useEffect(() => {
+        const db = firebase.firestore();
+        const albumRef = db.collection('albums').doc(song.album);
+        albumRef.get().then(doc => {
+            const album = doc.data();
+            const storage = firebase.storage();
+            const storageRef = storage.ref();
+            const imageRef = storageRef.child(`albums/${album.banner}`);
+            imageRef.getDownloadURL().then(url => {
+                setAlbumImage(url);
+            });
+        });
+    }, []);
+
+    return (
+        <div className='songs-slider__list-item'>
+            <div className='songs-slider__list-item-image' style={ { backgroundImage: `url(${albumImage})` } } />
+            <div className='songs-slider__list-item-info'>
+                <h3>{ song.name }</h3>
+            </div>
         </div>
     )
 }
