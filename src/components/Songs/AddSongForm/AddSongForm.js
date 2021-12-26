764384a73273205926703b2d34a62d114d869ea0
Juan Diego Luna <juandluna17@gmail.com>
2021-12-26 09:53:39 -0500

    Añadiendo una zona para subir la música

--- src/components/Songs/AddSongForm/AddSongForm.js
+++ src/components/Songs/AddSongForm/AddSongForm.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { Form, Input, Button, Icon, Dropdown, Label } from 'semantic-ui-react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Form, Input, Button, Icon, Dropdown, Label, Grid } from 'semantic-ui-react';
 import firebase from '../../../db/Firebase';
 
 import './AddSongForm.scss';
@@ -9,6 +10,7 @@ const db = firebase.firestore();
 const AddSongForm = ({ setShowModal }) => {
 
     const [albums, setAlbums] = useState([]);
+    const [file, setFile] = useState(null);
 
     useEffect(() => {
         db.collection('albums').get().then(snapshot => {
@@ -22,7 +24,17 @@ const AddSongForm = ({ setShowModal }) => {
             })
             setAlbums(albums);
         })
-    }, [])
+    }, []);
+
+    const onDrop = useCallback(acceptedFiles => {
+        setFile(acceptedFiles[0]);
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'audio/*',
+        noKeyboard: true,
+        onDrop
+    });
 
     const onSubmit = () => {
         console.log('Submit');
@@ -30,15 +42,30 @@ const AddSongForm = ({ setShowModal }) => {
 
     return (
         <Form className="add-song-form" onSubmit={ onSubmit }>
-            <Form.Field>
-                <Input placeholder='Nombre de la cancion' />
-            </Form.Field>
-            <Form.Field>
-                <Dropdown placeholder='Asigna la cancion a un album' noResultsMessage="No se encontraron albums" search lazyload fluid selection options={ albums } />
-            </Form.Field>
-            <Form.Field>
-                <Label>Subir cancion dropzone</Label>
-            </Form.Field>
+            <Grid>
+                <Grid.Column width={ 7 }>
+                    <div className="song-upload" { ...getRootProps() }>
+                        <input { ...getInputProps() } />
+                        <Icon name={ file ? 'file audio' : 'cloud upload' } className={ file && 'loaded' } />
+                        <div>
+                            <p>Arrastra tu cancion o haz click <span>aquí</span>.</p>
+                            {
+                                file && (
+                                    <p><span>{ file.name }</span></p>
+                                )
+                            }
+                        </div>
+                    </div>
+                </Grid.Column>
+                <Grid.Column width={ 9 }>
+                    <Form.Field>
+                        <Input placeholder='Nombre de la cancion' />
+                    </Form.Field>
+                    <Form.Field>
+                        <Dropdown placeholder='Asigna la cancion a un album' noResultsMessage="No se encontraron albums" search lazyload fluid selection options={ albums } />
+                    </Form.Field>
+                </Grid.Column>
+            </Grid>
             <Button type='submit' content='Agregar cancion' />
         </Form>
     )
