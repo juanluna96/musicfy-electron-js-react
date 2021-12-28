46b35cbe8bc9752cfca58ae62b228271c9d7edd1
Juan Luna <juandluna17@gmail.com>
2021-12-27 21:52:28 -0500

    Creando la estructura de la lista de canciones

--- src/components/Songs/ListSongs/ListSongs.js
+++ src/components/Songs/ListSongs/ListSongs.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { Table, Icon } from 'semantic-ui-react'
+
 
 import './ListSongs.scss'
 
 const ListSongs = ({ songs, albumImage }) => {
     return (
-        <div>
-
-        </div>
+        <Table inverted className="list-songs">
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell />
+                    <Table.HeaderCell>
+                        Nombre de la canción
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {
+                    songs.map(song => (
+                        <Table.Row key={ song.id }>
+                            <Table.Cell collapsing>
+                                <Icon name="play circle outline" />
+                            </Table.Cell>
+                            <Table.Cell>
+                                { song.name }
+                            </Table.Cell>
+                        </Table.Row>
+                    ))
+                }
+            </Table.Body>
+        </Table>
     )
 }
 
